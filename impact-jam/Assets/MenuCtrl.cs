cf7b85a367f719d6ed955ddc348e98f2580db85c
bloblodu69 <charlie.jeanneau@icloud.com>
2019-03-19 18:55:52 +0100

    Menu
    Add basic menu with play button and script for redirect all scene

---
+++ impact-jam/Assets/MenuCtrl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : MonoBehaviour
+{
+
+    public void LoadScene(string sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
