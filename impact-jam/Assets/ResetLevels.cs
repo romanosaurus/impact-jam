93e4d09c39a29cbce10421a6188f7b9fa70043b8
bloblodu69 <charlie.jeanneau@icloud.com>
2019-03-20 18:53:54 +0100

    Add Pause Menu In game with button
    Add  reset lvl & back home menu & quit level

---
+++ impact-jam/Assets/ResetLevels.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevels : MonoBehaviour
+{
+    static Scene m_scene;
+
+    void Update()
+    {
+        m_scene = SceneManager.GetActiveScene();
+    }
+
+    public void ResetActualLevel()
+    {
+        SceneManager.LoadScene(m_scene.name);
+    }
+}
