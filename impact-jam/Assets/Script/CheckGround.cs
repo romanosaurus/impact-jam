f0512909fabeed8e8e1ea1a003d9153f947b0021
romain <asauske@rom1.me>
2019-03-20 23:39:49 +0100

    + Added finish line

--- impact-jam/Assets/Script/CheckGround.cs
+++ impact-jam/Assets/Script/CheckGround.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class CheckGround : MonoBehaviour
 {
     public static bool Grounded;
+    private bool inWater = false;
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        Grounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        if (!inWater)
+            Grounded = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Water")
         {
-            GetComponent<Properties>().speed -= 75;
-
+            GetComponent<Properties>().speed -= 50;
+            Grounded = true;
+            inWater = true;
         }
     }
 
@@ -19,23 +32,10 @@ public class CheckGround : MonoBehaviour
     {
         if (collision.gameObject.tag == "Water")
         {
-            GetComponent<Properties>().speed += 75;
-
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D collider)
-    {
-
-        Grounded = true;
-    }
-
-    void OnCollisionExit2D(Collision2D collider)
-    {
-        if (collider.gameObject.tag == "Water")
-        {
             GetComponent<Properties>().speed += 50;
+            Grounded = false;
+            inWater = false;
         }
-        Grounded = false;
     }
+
 }
---
+++ impact-jam/Assets/Script/FinishLine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    private int playerIn = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            playerIn++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            playerIn--;
+    }
+
+    private void Update()
+    {
+        if (playerIn == 4)
+            Debug.Log("fini");
+    }
+}
