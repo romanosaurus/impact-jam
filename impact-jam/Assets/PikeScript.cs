e9ce2d5645c4338c577aaecb8d6fac53b4215c88
Guillaume JEANNE <guillaume.jeanne14@gmail.com>
2019-03-20 19:52:24 +0100

    added pike on scene and prefab

---
+++ impact-jam/Assets/PikeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PikeScript : MonoBehaviour
+{
+    private Scene m_Scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Scene = SceneManager.GetActiveScene();
+    }
+
+    void reloadScene(string sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        reloadScene(m_Scene.name);
+    }
+}
