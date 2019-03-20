709fdad08c8743dc3590b7a7129dc179ed7e8fcd
Guillaume JEANNE <guillaume.jeanne14@gmail.com>
2019-03-20 16:28:13 +0100

    added trap functionnal with death and scene reload also when falling

--- impact-jam/Assets/Script/Properties.cs
+++ impact-jam/Assets/Script/Properties.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Properties : MonoBehaviour
 {
     public float speed;
     public float speedSprint;
     public float jumpForce;
+    [SerializeField]
+    private GameObject Plateform;
+    private Scene m_Scene;
+
+    void Update()
+    {
+        m_Scene = SceneManager.GetActiveScene();
+        if (transform.position.y < Plateform.transform.position.y - 4)
+            reloadScene(m_Scene.name);
+    }
+
+    void reloadScene(string sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
 }
--- impact-jam/Assets/TrapScript.cs
+++ impact-jam/Assets/TrapScript.cs
@@ -1,22 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TrapScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject firstState;
+    [SerializeField]
+    private GameObject secondState;
+    [SerializeField]
+    private GameObject thirdState;
+    private float sinceTrapped;
+    private bool isTrapped;
+    private bool alreadyTrapped;
+    private Scene m_Scene;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTrapped = false;
+        alreadyTrapped = false;
+        sinceTrapped = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_Scene = SceneManager.GetActiveScene();
+        if (sinceTrapped == 0 && isTrapped && !alreadyTrapped)
+        {
+            firstState.SetActive(false);
+            secondState.SetActive(true);
+            alreadyTrapped = true;
+        }
+        if (alreadyTrapped || sinceTrapped > 0) {
+            sinceTrapped += Time.deltaTime;
+        }
+        if (sinceTrapped >= 2)
+        {
+            sinceTrapped = 0;
+            thirdState.SetActive(false);
+            firstState.SetActive(true);
+        }
+        if (sinceTrapped >= 0.42)
+        {
+            secondState.SetActive(false);
+            thirdState.SetActive(true);
+            if (isTrapped)
+                reloadScene(m_Scene.name);
+            isTrapped = false;
+            alreadyTrapped = false;
+        }
+    }
+
+    void reloadScene(string sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTrapped = false;
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        isTrapped = true;
+        
         Debug.Log("OnCollisionEnter2D");
     }
 }
