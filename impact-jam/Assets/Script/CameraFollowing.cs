43224532e4c7d128f07456f8510a02e652c145d2
romain <asauske@rom1.me>
2019-03-21 10:52:57 +0100

    * Modified Camera

--- impact-jam/Assets/Script/CameraFollowing.cs
+++ impact-jam/Assets/Script/CameraFollowing.cs
@@ -6,18 +6,12 @@ public class CameraFollowing : MonoBehaviour
 {
     public GameObject player;       //Public variable to store a reference to the player game object
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
-
+    public float FollowSpeed = 2f;
     // Use this for initialization
-    void Start()
+    private void Update()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
-    }
-
-    // LateUpdate is called after Update each frame
-    void LateUpdate()
-    {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        Vector3 newPosition = player.transform.position;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
     }
 }
