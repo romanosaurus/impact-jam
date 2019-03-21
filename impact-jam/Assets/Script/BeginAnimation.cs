1b18ed0bc20ceb96360aa608e52289a18806df63
romain <asauske@rom1.me>
2019-03-21 10:16:09 +0100

    * Modified Animation

---
+++ impact-jam/Assets/Script/BeginAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeginAnimation : MonoBehaviour
+{
+    public string textLevel;
+    public Text canvasText;
+    void Start()
+    {
+        canvasText.text = textLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
