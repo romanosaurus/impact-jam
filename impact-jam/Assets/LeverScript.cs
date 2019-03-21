42def56d0a39f26916d5ad380b759db9a82d69cd
Guillaume JEANNE <guillaume.jeanne14@gmail.com>
2019-03-21 19:36:46 +0100

    added bridge and lever in prefab and level1

---
+++ impact-jam/Assets/LeverScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject firstState;
+    [SerializeField]
+    private GameObject secondState;
+    [SerializeField]
+    private GameObject bridge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        firstState.SetActive(false);
+        secondState.SetActive(true);
+        bridge.SetActive(true);
+    }
+}
