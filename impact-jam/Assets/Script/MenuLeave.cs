dfbb319a60b774408abb6ec08668bb2b3dc80faa
bloblodu69 <charlie.jeanneau@icloud.com>
2019-03-22 14:16:30 +0100

    Add all

--- impact-jam/Assets/Script/MenuLeave.cs
+++ impact-jam/Assets/Script/MenuLeave.cs
@@ -6,8 +6,6 @@ public class MenuLeave : MonoBehaviour
 
     public void Quit()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
-//        for (int i = 0; i != 1000000000; i++);
         Application.Quit ();
     }
 }
